4b59a1e9b5348f58b51adf5674f0cccfe0d10de1
Emanoid <56281174+Emanoid@users.noreply.github.com>
2019-11-18 14:10:46 -0500

    Update IbintreeVisitor.java

--- IbintreeVisitor.java
+++ IbintreeVisitor.java
@@ -5,5 +5,5 @@ public interface IbintreeVisitor<X,R> {
 	//Purpose: To implement visitor class on a leafnode
 	public R leafnode(X val);
 	//Purpose: To implement visitor class on an Interiornode
-	public R interiornode(X val, bintree<X> l, bintree<X> r);	
+	public R interiornode(X val, R l, R r);	
 }
